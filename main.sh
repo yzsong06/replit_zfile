a0bec544cffbd49b2b0ef0aeaf6f5d566bf3d5f0
onesong <96452465+yzsong06@users.noreply.github.com>
2023-02-23 15:21:34 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,3 +1,4 @@
+#如需更新Zfile版本，请把根目录下的zfie-release.war删除
 if [ ! -f "zfile-release.war" ];then
 wget --no-check-certificate https://c.jun6.net/ZFILE/zfile-release.war      # 下载 zfile 最新版
 unzip zfile-release.war
